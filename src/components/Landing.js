402429235b04277ddd182fa89bd974a372c65bab
Michelle Yung <myung@email.arizona.edu>
2021-04-08 12:17:24 -0700

    add app info modals

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Card, Image, Segment, Divider } from 'semantic-ui-react';
+import { Button, Card, Image, Segment, Divider, Modal, Header } from 'semantic-ui-react';
 
 class Landing extends Component {
 
@@ -7,15 +7,26 @@ class Landing extends Component {
     super(props);
     
     this.state = {
+      modalOpen: false,
+      modalType: null,
     };
+  }
+
+  setOpen(modalOpen, modalType) {
+    this.setState({
+      modalOpen: modalOpen,
+      modalType: modalType
+    });
   }
   
   render() {
     
 
     const src = "ares.png";
+    const {modalOpen, modalType} = this.state;
 
     return (
+      
       <>
         <Image centered size='medium' src={"ua-logo.png"} as='a' href='https://www.superfund.arizona.edu/'/>
         
@@ -65,7 +76,7 @@ class Landing extends Component {
                 <Button as="a" href="https://dmac.pharmacy.arizona.edu/Ares/Develop" target="_blank" floated="right" primary>
                   Launch
                 </Button>
-                <Button as="a" floated="right">
+                <Button as="a" floated="right" onClick={() => this.setOpen(true, 'ares')}>
                   Info
                 </Button>
               </Card.Content>
@@ -79,14 +90,14 @@ class Landing extends Component {
                   {/* <span className='date'>Joined in 2015</span> */}
                 </Card.Meta>
                 <Card.Description>
-                  Launch nextGen sequencing analysis.
+                  Map RNA sequencing fastq files to reference genomes using STAR.
                 </Card.Description>
               </Card.Content>
               <Card.Content extra>
                 <Button as="a" href="https://demeter.pharmacy.arizona.edu" target="_blank" floated="right" primary>
                   Launch
                 </Button>
-                <Button as="a" floated="right">
+                <Button as="a" floated="right" onClick={() => this.setOpen(true, 'demeter')}>
                   Info
                 </Button>
               </Card.Content>
@@ -107,12 +118,91 @@ class Landing extends Component {
                 <Button as="a" href="https://dmac.pharmacy.arizona.edu/Hermes/Develop" target="_blank" floated="right" primary>
                   Launch
                 </Button>
-                <Button as="a" floated="right">
+                <Button as="a" floated="right" onClick={() => this.setOpen(true, 'hermes')}>
                   Info
                 </Button>
               </Card.Content>
             </Card>
           </Card.Group>
+
+          <Modal
+            onClose={() => this.setOpen(false, null)}
+            open={modalOpen && modalType === 'ares'}
+            size="tiny"
+          > 
+            <Modal.Header>Ares</Modal.Header>
+            <Modal.Content>
+              <Modal.Description>
+                <p>
+                  Ares performs DESeq2 analysis which analyzes count data from RNA sequences to detect differentially expressed genes using a negative binomial generalized linear model.
+                </p>
+                <p>
+                  By default, Deseq2 filters out tests with very little chance of being significant by using the mean of normalized counts as a filter statistic.
+                </p>
+                <p>
+                  Ares uses the Benjamini & Hochberg (1995) method implemented by DeSeq2 to control for family-wise error rate.
+                </p>
+                <p>
+                  Ares also pre-filters low count genes by removing rows where there are less than 10 reads.
+                </p>
+              </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={() => this.setOpen(false)}>
+                Close
+              </Button>
+              
+            </Modal.Actions>
+          </Modal>
+
+          <Modal
+            onClose={() => this.setOpen(false, null)}
+            open={modalOpen && modalType === 'demeter'}
+            size="tiny"
+          > 
+            <Modal.Header>Demeter</Modal.Header>
+            <Modal.Content>
+              <Modal.Description>
+                <p>Demeter has two modes: <b>RNA seq</b> and <b>DNA seq</b>.</p>
+                <h4>RNA Seq</h4>
+                <p>
+                  Runs <a href="https://github.com/alexdobin/STAR" target="_blank">STAR</a> to align RNA sequencing <code>.fastq</code> files stored on CyVerse to reference genomes. Additionally runs <a href="http://www.htslib.org" target="_blank">samtools</a> to generate <code>.bam</code> and <code>.bai</code> files. Outputs a <code>ReadsPerGene.tab</code> file that is passed to the Ares app for DESeq2 analysis.
+                </p>
+                <Divider></Divider>
+                <h4>DNA Seq</h4>
+                <p>Runs <a href="http://bowtie-bio.sourceforge.net/bowtie2/index.shtml" target="_blank">bowtie2</a> and <a href="http://www.htslib.org" target="_blank">samtools</a> to generate a sorted <code>.bam</code> and <code>.bai</code> file from <code>.fastq</code> files stored on CyVerse.</p>
+              </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={() => this.setOpen(false)}>
+                Close
+              </Button>
+              
+            </Modal.Actions>
+          </Modal>
+
+          <Modal
+            onClose={() => this.setOpen(false, null)}
+            open={modalOpen && modalType === 'hermes'}
+            size="tiny"
+          > 
+            <Modal.Header>Hermes</Modal.Header>
+            <Modal.Content>
+              <Modal.Description>
+                <p>
+                  Hermes description.
+                </p>
+              </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={() => this.setOpen(false)}>
+                Close
+              </Button>
+              
+            </Modal.Actions>
+          </Modal>
+
+          
         </div>
       </>
     );
